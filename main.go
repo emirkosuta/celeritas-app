236e258d42df6860fef8158a187610cfdc91c3e0
= <=>
2023-05-21 12:45:22 +0200

    clean up some files

--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
-import (
-	"myapp/middleware"
-
-	"github.com/emirkosuta/celeritas"
-)
-
-type application struct {
-	App        *celeritas.Celeritas
-	Middleware *middleware.Middleware
-}
-
 func main() {
 	c := initApplication()
-	c.App.ListenAndServe()
+	c.ListenAndServe()
 }
--- routes.go
+++ routes.go
@@ -11,16 +11,9 @@ import (
 func routes(app *celeritas.Celeritas, middleware *middleware.Middleware, handlers *handlers.Handlers) *chi.Mux {
 	// middleware must come before any routes
 
-	// add routes here
-	app.Routes.Get("/", handlers.Home)
-
 	//api
 	app.Routes.Route("/api", func(rt chi.Router) {
-		rt.Post("/cars", handlers.CarHandler.CreateCar)
-		rt.Get("/cars/{id}", handlers.CarHandler.GetCarById)
-		rt.Get("/cars", handlers.CarHandler.GetCarList)
-		rt.Put("/cars/{id}", handlers.CarHandler.UpdateCar)
-		rt.Delete("/cars/{id}", handlers.CarHandler.DeleteCar)
+
 	})
 
 	return app.Routes
